d2f7aa94b8e1676cdc2908178d165e299f1e1bc3
jose <jmaguero.ing@gmail.com>
2022-07-24 22:01:39 +0200

    promer commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=> {
+  res.send("hola server mi express")
+});
+
+app.get('/nueva-ruta', (req, res)=> {
+  res.send("hola mi nueva ruta")
+})
+
+app.get('/products', (req, res)=> {
+  res.json([{
+    name: 'product1',
+    price: 150
+  },
+  {
+    name: 'product2',
+    price: 350
+  }
+])
+});
+
+app.get('/products/:id',(req, res)=>{
+ //const product = req.params.id; esta es una forma,
+ const {id} = req.params;
+ /*esta forma es más moderna con la destructuracion de ECMAScript y viene a decir de todos los parametros del objeto params solo me interesa el id.*/
+
+  res.json(
+    {
+      id,
+      name: 'product2',
+      price: 350
+    }
+  );
+
+});
+
+app.get('/categories/:categoryid/products/:productId',(req, res)=>{
+  //const product = req.params.id; esta es una forma,
+  const {categoryid, productId} = req.params;
+  /*esta forma es más moderna con la destructuracion de ECMAScript y viene a decir de todos los parametros del objeto params solo me interesa el id.*/
+
+   res.json(
+     {
+      categoryid,
+       productId
+     }
+   );
+
+ });
+
+app.get('/users',(req, res)=>{
+  const { limit, offset } = req.query;
+  if(limit && offset){
+      res.json({
+        limit,
+        offset
+      });
+    }else{
+      res.send('no hay parametros1');
+    }
+ })
+
+app.listen(port, ()=> {
+  console.log('mi port' + port);
+});
+
+
+/*REST: Representational State Transfer
+Es una conveccion que se refiere a servicios web por protocolo HTTP
+
+Metodos:
+
+Get: Obtener
+Put: Modificar/Actualizar
+Patch: Modificar/Actualizar
+Post: Crear
+Delete: Eliminar
+Patch
+El método de solicitud HTTP PATCH aplica modificaciones parciales a un recurso.
+
+PATCH es algo análogo al concepto de “actualización” que se encuentra en CRUD, Una solicitud se considera un conjunto de instrucciones sobre cómo modificar un recurso. Contrasta esto con PUT; que es una representación completa de un recurso.PATCH
+
+Mo es necesariamente idempotente, aunque puede serlo. Contrasta esto con PUT; que siempre es idempotente.
+
+La palabra “idempotente” significa que cualquier número de solicitudes repetidas e idénticas dejará el recurso en el mismo estado.
+
+Por ejemplo, si un campo de contador de incremento automático es una parte integral del recurso, entonces un PUT lo sobrescribirá naturalmente (ya que sobrescribe todo), pero no necesariamente para .PATCH
+
+PATCH (como POST) puede tener efectos secundarios sobre otros recursos.
+
+PATCH - HTTP | MDN*/
+
+/*Los endpoints son las URLs de un API o un backend que responden a una petición. Los mismos entrypoints tienen que calzar con un endpoint para existir. Algo debe responder para que se renderice un sitio con sentido para el visitante. */
